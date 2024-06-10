1e790b16fcefea8f19ac911485249e27c1cd1681
KimHuiyeong <kkhhyy1@naver.com>
2024-06-10 21:29:21 +0900

    과제 숫자야구게임 다시 풀기 -> 입력숫자(1~9까지 아닐 때 예외, 숫자 이외일 때 예외) 제외하고 문제 풀기 완료

---
+++ CodingTest/src/Homework/NumberBaseball.java
@@ -0,0 +1,92 @@
+package Homework;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class NumberBaseball {
+    public static void main(String[] args) {
+        NumberBaseball numberBaseball = new NumberBaseball();
+        numberBaseball.baseballPlay(3);
+    }
+
+    public int[] randomPlayer(int number) {
+        int[] randomNumber = new int[number];
+        for(int i : randomNumber){
+            randomNumber[i] = (int) (Math.random() * 9); //랜덤함수 범위 인터넷에서 찾아봄
+        }
+
+        for(int i = 0; i < randomNumber.length - 1; i++) {
+            for(int j = 0; j < randomNumber.length; j++) {
+                if(i < j) {
+                    if(randomNumber[i] == randomNumber[j]) {
+                        randomNumber[j] = (int) (Math.random() * 9);
+                        j--;
+                    }
+                }
+            }
+        }
+        System.out.println("random : " + Arrays.toString(randomNumber));
+        return randomNumber;
+    }
+
+    public int[] inputPlayer(int number) {
+        Scanner scanner = new Scanner(System.in);
+        int[] inputNumber = new int[number];
+
+        for(int i = 0; i < inputNumber.length; i++) {
+            inputNumber[i] = scanner.nextInt();
+        }
+
+        for(int i = 0; i < inputNumber.length; i++) {
+            for(int j = 0; j < inputNumber.length; j++) {
+                if(i < j) {
+                    if(inputNumber[i] == inputNumber[j]) {
+                        inputNumber[j] = scanner.nextInt();
+                        j--;
+                    }
+                }
+            }
+        }
+        System.out.println("input : " + Arrays.toString(inputNumber));
+        return inputNumber;
+    }
+
+    public void baseballPlay(int number) {
+        NumberBaseball numberBaseball = new NumberBaseball();
+        int strike = 0;
+        int ball = 0;
+
+        System.out.println("숫자를 입력하세요.");
+
+        int[] randomNumber = numberBaseball.randomPlayer(number);
+
+        while(true) {
+            int[] inputNumber = numberBaseball.inputPlayer(number);
+
+            for(int i = 0; i < number; i++) {
+                if (randomNumber[i] == inputNumber[i]) {
+                    strike++;
+                }
+            }
+
+            for(int i = 0; i < randomNumber.length; i++) {
+                for(int j = 0; j < inputNumber.length; j++) {
+                    if(randomNumber[i] == inputNumber[j]) {
+                        if(i != j) {
+                            ball++;
+                        }
+                    }
+                }
+            }
+            System.out.println("strike : " + strike);
+            System.out.println("ball : " + ball);
+            if(strike == number) {
+                break;
+            }
+            strike = 0;
+            ball = 0;
+        }
+
+        System.out.println("정답입니다.");
+    }
+}
