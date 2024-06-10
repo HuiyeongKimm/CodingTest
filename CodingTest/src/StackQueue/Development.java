279ae52eb18544965d4e6ba1eb73dfb081efb662
KimHuiyeong <kkhhyy1@naver.com>
2024-06-10 23:43:52 +0900

    프로그래머스 코딩테스트 스택큐 레벨2 기능개발 Queue로 풀었다 !!! 전에는 답은 맞지만 코드 풀이과정에서 틀려서 코테 통과를 못했는데 이번엔 통과까지 함 ,, ㅜㅜ 하는 과정에서 day 변수 추가하고 speeds[speeds.length - pq.size()] 이거 매우 신세계 ,,,! 종종 기억해두고 사용하고 싶음

--- CodingTest/src/StackQueue/Development.java
+++ CodingTest/src/StackQueue/Development.java
@@ -1,4 +1,4 @@
-package Hash;
+package StackQueue;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -8,27 +8,44 @@ public class Development {
     public static void main(String[] args) {
         Development development = new Development();
         List<Integer> solutionList = development.solution(new int[]{95, 90, 99, 99, 80, 99},
-                new int[]{1, 2, 3, 4, 5, 6});
+                new int[]{1, 1, 1, 1, 1, 1});
         System.out.println("solution = " + solutionList);
     }
 
     public List<Integer> solution(int[] progresses, int[] speeds) {
         List<Integer> answerList = new LinkedList<>();
         Queue<Integer> pq = new LinkedList<>();
+        int[] days = new int[progresses.length];
+        int count = 1;
+        int day = 1;
+
         for(int i : progresses) {
             pq.add(i);
         }
-//        while(!pq.isEmpty()) {
-//            System.out.print("pq :" +pq.poll() );
-//            System.out.println("   speed = " + speeds[speeds.length - pq.size() - 1]); //speeds
-//        }
-        int day = 1;
+
         while(!pq.isEmpty()) {
             if(pq.peek() + speeds[speeds.length - pq.size()] * day >= 100) {
+                days[speeds.length - pq.size()] = day;
                 pq.poll();
+                day = 1;
             }
             day++;
         }
+
+        for(int i = 0; i < days.length - 1; i++) {
+            if(days[i] > days[i + 1]) {
+                days[i + 1] = days[i];
+            }
+
+            if(days[i] == days[i + 1]) {
+                count++;
+            } else {
+                answerList.add(count);
+                count = 1;
+            }
+        }
+        answerList.add(count);
+
         return answerList;
     }
 }
