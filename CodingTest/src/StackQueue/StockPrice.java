b2203da25a4254c44130ce48200a1b5c34535863
KimHuiyeong <kkhhyy1@naver.com>
2024-06-16 22:18:46 +0900

    프로그래머스 코딩테스트 스택큐 레벨2 주식가격 더 나은 방식이 있는지 찾아보는 중

--- CodingTest/src/StackQueue/StockPrice.java
+++ CodingTest/src/StackQueue/StockPrice.java
@@ -12,20 +12,24 @@ public class StockPrice {
     }
 
     public int[] solution(int[] prices) {
+        //answer라는 int형 배열을 선언함(배열 prices 길이만큼 배열을 만듬)
         int[] answer = new int[prices.length];
+        //Queue 클래스인 queue도 선언함
         Queue<Integer> queue = new LinkedList<>();
 
+        //prices에 있는 데이터를 queue에다 하나씩 복사함
         for(int i : prices) {
             queue.add(i);
         }
 
         while(!queue.isEmpty()) {
-            int count = queue.size() - 1;
+            //prices에 있는 데이터보다
+            int count = 0;
             int qa = queue.poll();
 
             for(int i = 0; i < queue.size(); i++) {
-                if(qa > queue.peek()) {
-                    count--;
+                if(qa <= queue.peek()) {
+                    count++;
                 }
                 queue.add(queue.poll());
             }
