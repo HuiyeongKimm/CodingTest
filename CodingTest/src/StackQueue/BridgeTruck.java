8151c47bf32a1e36185efeae710e3f3f2de2cf54
KimHuiyeong <kkhhyy1@naver.com>
2024-06-16 22:17:45 +0900

    프로그래머스 코딩테스트 스택큐 레벨2 다리를 건너는 트럭 Queue 두개로 문제 푸는 중 -> 27줄에 waitQueue.peek()을 쓰면 NullpointException 발생 ,,, 어떻게 해야 안 나는지 도무지 생각이 안 나서 중간에 커밋함

--- CodingTest/src/StackQueue/BridgeTruck.java
+++ CodingTest/src/StackQueue/BridgeTruck.java
@@ -7,20 +7,33 @@ public class BridgeTruck {
     public static void main(String[] args) {
         BridgeTruck bridgeTruck = new BridgeTruck();
         int solution = bridgeTruck.solution(2, 10, new int[]{7, 4, 5, 6});
+        System.out.println("solution = " + solution);
     }
 
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
-        Queue<Integer> queue = new LinkedList<>();
+        int count = 0;
+        Queue<Integer> waitQueue = new LinkedList<>();
+        Queue<Integer> truckQueue = new LinkedList<>();
 
         for(int i : truck_weights) {
-            queue.add(i);
+            waitQueue.add(i);
         }
 
-        while(!queue.isEmpty()) {
-            if(queue.peek() <= weight) {
+        while(!waitQueue.isEmpty() || !truckQueue.isEmpty()) {
+            truckQueue.add(waitQueue.poll());
 
+            if (truckQueue.stream().mapToInt(Integer::intValue).sum()
+                    + waitQueue.peek() < weight) {
+                answer += bridge_length;
+                count += bridge_length;
             }
+
+            if(count == bridge_length) {
+                truckQueue.poll();
+                count = 0;
+            }
+            count++;
             answer++;
         }
         return answer;
