7f6e047f5395447a79f57a4f31a752fb4d64b34d
KimHuiyeong <kkhhyy1@naver.com>
2024-08-13 00:20:04 +0900

    과제 야구게임 좀 더 가꿈

--- CodingTest/src/Homework/NumberBaseball.java
+++ CodingTest/src/Homework/NumberBaseball.java
@@ -6,19 +6,19 @@ import java.util.Scanner;
 public class NumberBaseball {
     public static void main(String[] args) {
         NumberBaseball numberBaseball = new NumberBaseball();
-        numberBaseball.baseballPlay(3);
+        numberBaseball.play(3);
     }
 
     public int[] randomPlayer(int number) {
         int[] randomNumber = new int[number];
-        for(int i : randomNumber){
+        for (int i : randomNumber) {
             randomNumber[i] = (int) (Math.random() * 9); //랜덤함수 범위 인터넷에서 찾아봄
         }
 
-        for(int i = 0; i < randomNumber.length - 1; i++) {
-            for(int j = 0; j < randomNumber.length; j++) {
-                if(i < j) {
-                    if(randomNumber[i] == randomNumber[j]) {
+        for (int i = 0; i < randomNumber.length - 1; i++) {
+            for (int j = 0; j < randomNumber.length; j++) {
+                if (i < j) {
+                    if (randomNumber[i] == randomNumber[j]) {
                         randomNumber[j] = (int) (Math.random() * 9);
                         j--;
                     }
@@ -33,14 +33,14 @@ public class NumberBaseball {
         Scanner scanner = new Scanner(System.in);
         int[] inputNumber = new int[number];
 
-        for(int i = 0; i < inputNumber.length; i++) {
+        for (int i = 0; i < inputNumber.length; i++) {
             inputNumber[i] = scanner.nextInt();
         }
 
-        for(int i = 0; i < inputNumber.length; i++) {
-            for(int j = 0; j < inputNumber.length; j++) {
-                if(i < j) {
-                    if(inputNumber[i] == inputNumber[j]) {
+        for (int i = 0; i < inputNumber.length; i++) {
+            for (int j = 0; j < inputNumber.length; j++) {
+                if (i < j) {
+                    if (inputNumber[i] == inputNumber[j]) {
                         inputNumber[j] = scanner.nextInt();
                         j--;
                     }
@@ -51,28 +51,28 @@ public class NumberBaseball {
         return inputNumber;
     }
 
-    public void baseballPlay(int number) {
+    public void play(int number) {
         NumberBaseball numberBaseball = new NumberBaseball();
-        int strike = 0;
-        int ball = 0;
-
         System.out.println("숫자를 입력하세요.");
 
         int[] randomNumber = numberBaseball.randomPlayer(number);
 
-        while(true) {
+        while (true) {
+            int strike = 0;
+            int ball = 0;
+
             int[] inputNumber = numberBaseball.inputPlayer(number);
 
-            for(int i = 0; i < number; i++) {
+            for (int i = 0; i < number; i++) {
                 if (randomNumber[i] == inputNumber[i]) {
                     strike++;
                 }
             }
 
-            for(int i = 0; i < randomNumber.length; i++) {
-                for(int j = 0; j < inputNumber.length; j++) {
-                    if(randomNumber[i] == inputNumber[j]) {
-                        if(i != j) {
+            for (int i = 0; i < randomNumber.length; i++) {
+                for (int j = 0; j < inputNumber.length; j++) {
+                    if (randomNumber[i] == inputNumber[j]) {
+                        if (i != j) {
                             ball++;
                         }
                     }
@@ -80,11 +80,9 @@ public class NumberBaseball {
             }
             System.out.println("strike : " + strike);
             System.out.println("ball : " + ball);
-            if(strike == number) {
+            if (strike == number) {
                 break;
             }
-            strike = 0;
-            ball = 0;
         }
 
         System.out.println("정답입니다.");
