ac974b2f48cc96d53d4b0c4fb9399bcff731e8e4
KimHuiyeong <kkhhyy1@naver.com>
2024-08-27 21:38:52 +0900

    그디어 문제 품 !!!!!! 여기선 두 수를 더했을 때 limit보다 크면 두 수가 다 움직이면 안 되고 한 수만 움직여서 그 다음 넘어가야함 ex) people[0] + people[2] > limit이면 그 다음 people[0] + peopel[1] 로 넘어가야되는데 그 전에 코드는 people[1] + people[1]로 넘어감 그럼 모든 수를 비교할 수가 없음 ㅠ 그래서 limit보다 크면 i-- 함으로써 한 수는 다시 계산하고 나머지 수는 다른 수로 해서 모든 수를 비교함. 하나는 index에다 people 길이 - 1로 해놓고 if 문에다가는 index로 바꿔서 했다 그 이유는 처음 index를  for문에 i < index로 했는데 그럼 people[0] + people[0]도 비교하기 때문에 peopl 길이에다가 1를 빼서 for 문의 리미트를 걸어준다.

--- CodingTest/src/Greedy/LifeBoat.java
+++ CodingTest/src/Greedy/LifeBoat.java
@@ -5,20 +5,21 @@ import java.util.Arrays;
 public class LifeBoat {
     public static void main(String[] args) {
         LifeBoat lifeBoat = new LifeBoat();
-        int solution = lifeBoat.solution(new int[]{70, 50, 80, 50, 5, 5, 10, 20, 30, 40, 40, 40, 30, 30}, 100);
+        int solution = lifeBoat.solution(new int[]{70, 50, 80}, 100);
         System.out.println("solution = " + solution);
     }
 
     public int solution(int[] people, int limit) {
         int answer = people.length;
         Arrays.sort(people);
-        int index = people.length;
+        int index = people.length - 1;
 
         for (int i = 0; i < index; i++) {
-            if ((people[i] + people[index - 1]) <= limit) {
+            if ((people[i] + people[index]) <= limit) {
                 answer--;
                 index--;
             } else {
+                i--;
                 index--;
             }
         }
