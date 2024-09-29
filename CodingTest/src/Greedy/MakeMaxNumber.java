a9d2724d9720b3911b8a337b06de5c49b962dfc1
KimHuiyeong <kkhhyy1@naver.com>
2024-09-29 19:23:34 +0900

    프로그래머스 코딩테스트 레벨2 큰 수 만들기 코멘트만 살짝 바꿈

--- CodingTest/src/Greedy/MakeMaxNumber.java
+++ CodingTest/src/Greedy/MakeMaxNumber.java
@@ -11,7 +11,7 @@ public class MakeMaxNumber {
         return makeNumber(number, k);
     }
 
-    public String makeNumber(String number, int k) { //
+    public String makeNumber(String number, int k) {
         StringBuilder sb = new StringBuilder(number);
 
         while (k > 0) {
@@ -35,13 +35,13 @@ public class MakeMaxNumber {
         return sb.toString();
     }
 
-    public int compareChar(int comNum1, int comNum2, int i) { // comNum1,2 숫자 비교 후 작은 수 중 십의 자리와 일의 자리 수 비교 후 작은 수 반환
+    public int compareChar(int comNum1, int comNum2, int i) { // comNum1,2 숫자 비교 후 작은 수 중 십의 자리와 일의 자리 수 비교 후 작은 수의 인덱스 번호 반환
         char minChar1 = (Math.min(comNum1, comNum2) + "").charAt(0);
         char minChar2 = (Math.min(comNum1, comNum2) + "").charAt(1);
         int minNum1 = Integer.parseInt(String.valueOf(minChar1));
         int minNum2 = Integer.parseInt(String.valueOf(minChar2));
 
-        if (comNum1 <= comNum2) {
+        if (comNum1 <= comNum2) { // 작은 수의 인덱스 번호 찾고 반환하기
             if (minNum1 <= minNum2) {
                 return i;
             } else {
